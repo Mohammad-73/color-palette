6c7fdf8f2dc776ff735423049477f7e7b9dc3fd8
Mohammad-73 <m.khalili7310@gmail.com>
2021-08-03 15:44:21 +0430

    create a contentful service

---
+++ src/service/contentful.js
@@ -0,0 +1,8 @@
+import * as SDK from "contentful";
+
+const contentful = SDK.createClient({
+  space: "q84fl2laoe8y",
+  accessToken: "ktgauRfNyvQCX16GXWcZHOvyL_1g1xVoriaqhPb2vlU",
+});
+
+export default contentful;
